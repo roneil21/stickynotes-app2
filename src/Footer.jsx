01a6eb004f41c8961ceea3a30a30546cfd9f2880
roneil21 <roneilalgara@gmail.com>
2023-07-23 15:48:08 +0800

    first commit

---
+++ src/Footer.jsx
@@ -0,0 +1,11 @@
+function Footer() {
+  const currentYear = new Date().getFullYear();
+  return (
+    
+      <footer>
+        <p>Made by Roneil Algara {currentYear}</p>
+      </footer>
+    
+  );
+}
+export default Footer;
---
+++ src/Header.jsx
@@ -0,0 +1,11 @@
+function Header(){
+    return (
+    <div >
+    <header>
+    <h1>Sticky Notes</h1>
+    </header>
+    </div>
+    )
+}
+
+export default Header;
